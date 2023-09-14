404c642f2ddf3bf2fe22de67f2a6ccc141dcfa9c
AmritSanjeev2007 <84622163+AmritSanjeev2007@users.noreply.github.com>
2023-09-15 04:36:00 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,23 +1,51 @@
-export const Loader = 
+import { generate_random_image } from "./random_img_gen.js";
+
+
+class ParagraphData
 {
-    hasLoaded: false,
+    /** @type {HTMLImageElement | SVGSVGElement | null} */
+    image = null;
+    /** @type {string[]} */
+    paragraphs = [];
 
-    content: '',
-    n_paragraphs: 0,
-
-    /** @type {((data:any)=>void)[]} */
-    callbackList: [],
-    /** @type {any[]} */
-    callbackData: [],
+    constructor(image, paragraphs)
+    {
+        this.image = image;
+        this.paragraphs = paragraphs;
+    }
 };
 
-document.addEventListener('DOMContentLoaded', function(){
-    fetch('/information.txt').then(function(res){
-        res.text().then(function(information){
-            Loader['hasLoaded'] = true;
-            Loader['content'] = information;
-            Loader['n_paragraphs'] = information.match(/[^\r]/g).join('').split('\n\n').length;
-            Loader['callbackList'].forEach((v,i) => v(Loader['callbackData'][i]));
-        });
-    });
-});
+/** @type {ParagraphData[]} */
+export const DataList = [];
+
+/**
+ * @param {HTMLImageElement | SVGSVGElement | null} image 
+ * @param {string[]} paragraphs 
+ */
+function addData(image, paragraphs) 
+{
+    DataList.push(new ParagraphData(image, paragraphs));
+}
+
+function loadDataList()
+{
+    DataList.forEach(function(data){
+        const informationContainer = document.createElement('div');
+        informationContainer.className = `information-container`;
+
+        if(data.image !== null) informationContainer.appendChild(data.image);
+
+        const informationPart = informationContainer.appendChild(document.createElement('div'));
+        informationPart.className = 'information-part';
+
+        data.paragraphs.forEach(function(paragraph){
+            informationPart.appendChild(document.createElement('p')).innerText = paragraph;
+        })
+
+        document.querySelector('.information').appendChild(informationContainer);
+    })
+}
+
+addData(generate_random_image(100, 100), [`Rhonda prided herself on always taking the path less traveled. She'd decided to do this at an early age and had continued to do so throughout her entire life. It was a point of pride and she would explain to anyone who would listen that doing so was something that she'd made great efforts to always do. She'd never questioned this decision until her five-year-old niece asked her, "So, is this why your life has been so difficult?" and Rhonda didn't have an answer for her.`])
+addData(generate_random_image(100,100), ["Why?"])
+loadDataList();
